06c3f58033e6611c6ed8b148f692e349291bb006
rasamkamyar <rasamkamyar@gamil.com>
2024-02-26 16:26:23 +0330

    new edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,13 @@
 import { useState } from "react";
 import Style from "./App.module.css";
-import { Link, Route, Routes } from "react-router-dom";
-import StartPage from "./components/Products";
 import Products from "./components/Products";
 
 function App() {
   const [isStarted, setIsStarted] = useState(true);
 
-  function handleClick() {
-    setIsStarted(false);
-  }
-
   return isStarted ? (
     <div className={Style.container}>
-      <button
-        style={{
-          position: "fixed",
-          top: "85%",
-          left: "14%",
-          borderRadius: "10px",
-          fontSize: "30px",
-          padding: "20px 70px",
-        }}
-        onClick={handleClick}
-      >
+      <button className={Style.startBtn} onClick={() => setIsStarted(false)}>
         Get Started
       </button>
     </div>
