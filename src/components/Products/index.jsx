2499947ab5473c9326b219d7322b3511cc86a5eb
RASAM KAMYAR <rasamkamyar@gmail.com>
2024-03-03 17:02:05 +0330

    create search bar component

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -2,35 +2,16 @@ import { useContext, useRef } from "react";
 import Style from "./index.module.css";
 import ProductContext from "../../context/ProductContext";
 import FilterBar from "../FilterBar";
+import SearchBar from "../SearchBar";
 import Header from "../Header";
 
 function Products() {
-  const { products, setProducts, temp } = useContext(ProductContext);
-  const serachRef = useRef(null);
-
-  function handleSearch() {
-    const value = serachRef.current.value;
-    const searchedProduct = temp.filter((item) => {
-      return item.title.toLowerCase().includes(value.toLowerCase());
-    });
-    setProducts(searchedProduct);
-  }
+  const { products } = useContext(ProductContext);
 
   return (
     <div>
       <Header />
-      <div className={Style.search}>
-        <input
-          type="text"
-          style={{ width: "100%" }}
-          ref={serachRef}
-          onChange={handleSearch}
-        />
-        <button style={{ backgroundColor: "purple", border: "none" }}>
-          <i className={`fa fa-search  ${Style.searchIcon}`}></i>
-        </button>
-      </div>
-
+      <SearchBar />
       <FilterBar />
 
       {products.map((product) => {
---
+++ src/components/SearchBar/index.jsx
@@ -0,0 +1,32 @@
+import { useContext, useRef } from "react";
+import Style from "./index.module.css";
+import ProductContext from "../../context/ProductContext";
+
+function SearchBar() {
+  const { setProducts, temp } = useContext(ProductContext);
+  const serachRef = useRef(null);
+
+  function handleSearch() {
+    const value = serachRef.current.value;
+    const searchedProduct = temp.filter((item) => {
+      return item.title.toLowerCase().includes(value.toLowerCase());
+    });
+    setProducts(searchedProduct);
+  }
+
+  return (
+    <div className={Style.search}>
+      <input
+        type="text"
+        style={{ width: "100%" }}
+        ref={serachRef}
+        onChange={handleSearch}
+      />
+      <button style={{ backgroundColor: "purple", border: "none" }}>
+        <i className={`fa fa-search  ${Style.searchIcon}`}></i>
+      </button>
+    </div>
+  );
+}
+
+export default SearchBar;
